248852c116efebc82de48bd0d66435556e69cb22
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-15 20:10:55 +0100

    To add video commentaries

--- 01-main.dart
+++ 01-main.dart
@@ -8,8 +8,13 @@ void main() {
   sdasd
   */
   
-  String nombre;
+  // Ways to concatenate Strings:
+  String nombre = "Alfredo"; // Here you can see that you can use ' or "
 
+  // 1.Classic one
+  print('Hola ' + nombre);
+
+  // 2.Dart one
   print('Hola $nombre');
   
 }
