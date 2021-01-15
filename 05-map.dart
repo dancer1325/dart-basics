13fba96cadd26f215f24378d9adcc7a2c1315cdb
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-16 00:14:20 +0100

    To add map commentaries

--- 05-map.dart
+++ 05-map.dart
@@ -1,8 +1,17 @@
 
 void main() {
   
+Map dynamicPerson = {
+    'nombre' : 'Carlos',
+    'edad' : 32,
+    'soltero': true
+  };
+
+  //To get a value isn't as another languages using a . because it only shows the specific methods for the Map class
+  //print(dynamicPerson.nombre);
+
+  //To create a variable with value the key to extract
   String propiedad = 'soltero';
-  
   
   Map<String, dynamic> persona = {
     'nombre' : 'Carlos',
@@ -20,10 +29,22 @@ void main() {
     2: 'Peter',
     9: 'Strange'
   };
-  
+
+  //Once you have specified an specific type for the key or the value, you can't introduce any type of variable
+  // Map<int, String> wrongMap = {
+  //   1: 'Tony',
+  //   2: 'Peter',
+  //   9: 'Strange',
+  //   "Alfredo": 4
+  // };
+
+
+  //To add new element to the map
   personas.addAll( { 4: 'Banner'  } );
   
   print( personas );
+
+  //To get the value of one entrance, you need to specify the key
   print( personas[2] );
   
   
