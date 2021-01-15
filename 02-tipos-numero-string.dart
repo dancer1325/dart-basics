82541b81d003971efcaf110aa9f90c53b7cfcac3
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-15 20:30:50 +0100

    To add video commentaries

--- 02-tipos-numero-string.dart
+++ 02-tipos-numero-string.dart
@@ -2,14 +2,29 @@ void main() {
   
 //   	Números
   int empleados = 10;
+  
+  //Error defining variables uncorrectly 
+  //int empleados = 10.0;
+
   double pi = 3.141592;
+
+  //If we don't specify the type of variable
   var numero = 1.0;
   
   print( '$empleados - $pi - $numero' );
   
 //   String - Cadenas de caracteres
   String nombre = 'Tony';
-  print(nombre);
+  String nombreWithDouble = "Tony";
+  String nombreWithDoubleAndSimpleInTheMiddle1Try = "To'ny";
+  String nombreWithDoubleAndSimpleInTheMiddle2Try = "To\'ny";
+  String nombreWithSimpleAndDoubleInTheMiddle1Try = 'To"ny';
+  String nombreWithSimpleAndDoubleInTheMiddle2Try = 'To\"ny';
+
+  print(nombreWithDoubleAndSimpleInTheMiddle1Try + " or " + nombreWithDoubleAndSimpleInTheMiddle2Try);
+  print(nombreWithSimpleAndDoubleInTheMiddle1Try + " or " + nombreWithSimpleAndDoubleInTheMiddle2Try);
+
+  print(nombre + ' or ' + nombreWithDouble);
   print(nombre[0]);
   print(nombre[ nombre.length - 1 ]);
   
