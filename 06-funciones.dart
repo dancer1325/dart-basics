94d77a8ad7c837b3d0e856c8b6bad4f45e01102f
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-16 00:33:16 +0100

    To add functions commentaries

--- 06-funciones.dart
+++ 06-funciones.dart
@@ -6,10 +6,21 @@ void main() {
   
 }
 
+//Function returning the default type (dynamic)
+defaultTypeToReturn() {
+  print('Default type to return');
+}
 
+//The argument's type of the function can be not specified
+String saludar( texto,  nombre ) {
+  return '$texto $nombre';
+}
+
+//To specify the type of the arguments and their name
 String saludar({ String texto, String nombre }) {
 //   print('Hola');
   return '$texto $nombre';
 }
 
+//Arrow functions
 String saludar2({ String texto, String nombre }) => '$texto $nombre';
