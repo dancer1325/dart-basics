c8e5171eeee68a97d622b430517174c30e27920f
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-16 11:48:21 +0100

    To add extend commentaries

--- 11-extends.dart
+++ 11-extends.dart
@@ -1,6 +1,7 @@
 void main() {
   
   final superman = new Heroe();
+  //As Heroe extends to Personaje, it contains also those properties
   superman.nombre = 'Clark Kent';
   
   final luthor = new Villano();
@@ -9,8 +10,13 @@ void main() {
   
 }
 
+//It's not necessary that the class is abstract to have the posibility to extend or to implement
+// class Personaje {
+//   String poder;
+//   String nombre;
+// }
 
-
+//It's done abstract in order to avoid creating instance of Personaje
 abstract class Personaje {
   String poder;
   String nombre;
