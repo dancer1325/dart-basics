45a40724eb04c4a3262207af46667cd75db34898
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-16 12:58:27 +0100

    To add Async and Await commentaries

--- 14-async-await.dart
+++ 14-async-await.dart
@@ -3,6 +3,16 @@ void main() async {
   
 	print('Estamos a punto de pedir datos');
   
+  //An alternative when you have got async calls, is to change the next code into then; to be executed when it spends the time
+// httpGet('https://api.nasa.com/aliens').then( (data) {
+    
+//     print( data );
+//     print('Ultima línea');  
+//   });
+
+  //We get an error because httpGet returns Future<String>
+  // String data = httpGet('https://api.nasa.com/aliens');
+
   String data = await httpGet('https://api.nasa.com/aliens');
   
   print( data );
@@ -19,6 +29,13 @@ Future<String> httpGet(String url) {
   });
   
 }
+
+//It's not allowed creating async constructors
+// class Heroe {
+//   Heroe() async {
+
+//   }
+// }
 
 
 
