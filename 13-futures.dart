dd9f1a509bf3dd8670f3e947601264c474a79b8f
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-16 12:44:47 +0100

    To add Futures commentaries

--- 13-futures.dart
+++ 13-futures.dart
@@ -3,17 +3,25 @@ void main() {
   
 	print('Estamos a punto de pedir datos');
   
+  //Allows using the resolution of the Future
   httpGet('https://api.nasa.com/aliens').then( (data) {
     
     print( data );
     
   });
   
-  
+  //The execution continues, without waiting for the another response
   print('Ultima línea');
 }
 
-
+//If we don't specify the type, it will be dynamic
+// Future httpGet(String url) {
+  
+//   return Future.delayed( new Duration( seconds: 4 ), () {
+//     return 'Hola Mundo';
+//   });
+  
+// }
 
 Future<String> httpGet(String url) {
   
