dbf3702cd9273a59b6196e65f7aa21c787a5d7c9
dancer1325 <alfredotic0809@gmail.com>
2022-07-12 17:40:32 +0200

    refactor(dart-basics) : remove extra blank lines (Section2)

--- 03-bool-condicion.dart
+++ 03-bool-condicion.dart
@@ -1,17 +1,14 @@
 // Default entrance method executed by all .dart files
 void main() {
-  
-  
+
   bool activado = true;
   print(activado);
-  
   
   if ( !activado ) {
     print('El motor esta funcionando');
   } else {
     print('Está apagado');
   }
-  
   
   // Same part of the code, but without sense since the variable is a boolean
   if ( activado == true ) {
