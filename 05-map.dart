de4fae71f69c23f3d5419ee47a33c14a95e629a2
dancer1325 <alfredotic0809@gmail.com>
2022-07-12 18:10:18 +0200

    refactor(dart-basics) : add cases and commentaries about map (Section2)

--- 05-map.dart
+++ 05-map.dart
@@ -1,34 +1,32 @@
 // Default entrance method executed by all .dart files
 void main() {
-  
-Map dynamicPerson = {
-    'nombre' : 'Carlos',
-    'edad' : 32,
-    'soltero': true
-  };
+  // Dynamic key and value types
+  Map dynamicPerson = {'nombre': 'Carlos', 'edad': 32, 'soltero': true};
 
   //To get a value isn't as another languages using a . because it only shows the specific methods for the Map class
   //print(dynamicPerson.nombre);
 
-  //To create a variable with value the key to extract
+  // Inferring the type without specifying the key / value 's types, are dynamic
+  print('dynamicPerson.runtimeType $dynamicPerson.runtimeType');
+
+  //Variable with the value of the map's key
   String propiedad = 'soltero';
-  
+
   Map<String, dynamic> persona = {
-    'nombre' : 'Carlos',
-    'edad'   : 32,
+    'nombre': 'Carlos',
+    'edad': 32,
     'soltero': true
   };
-  
-  print( persona['nombre'] );
-  print( persona['edad'] );
-  print( persona[ propiedad ] );
-  
-  
-  Map<int, String> personas = {
-    1: 'Tony',
-    2: 'Peter',
-    9: 'Strange'
-  };
+  // TODO: How does an interpolation with ' or " work?
+  // print('persona[\'nombre\'\] $persona\[\'nombre\'\]');
+  var name = persona['nombre'];
+  var age = persona['edad'];
+  var isSingle = persona['soltero'];
+  print('name $name');
+  print('age $age');
+  print('isSingle $isSingle');
+
+  Map<int, String> personas = {1: 'Tony', 2: 'Peter', 9: 'Strange'};
 
   //Once you have specified an specific type for the key or the value, you can't introduce any type of variable
   // Map<int, String> wrongMap = {
@@ -38,15 +36,11 @@ Map dynamicPerson = {
   //   "Alfredo": 4
   // };
 
-
   //To add new element to the map
-  personas.addAll( { 4: 'Banner'  } );
-  
-  print( personas );
+  personas.addAll({4: 'Banner'});
+
+  print('personas $personas');
 
   //To get the value of one entrance, you need to specify the key
-  print( personas[2] );
-  
-  
+  print(personas[2]);
 }
-
