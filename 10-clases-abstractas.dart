db48fa1bd7ffd84a585b2be31b50a1f1d55faa36
dancer1325 <alfredotic0809@gmail.com>
2022-07-16 17:11:10 +0200

    refactor(dart-basics) : clean and add commentaries about abstract class (Section2)

--- 10-clases-abstractas.dart
+++ 10-clases-abstractas.dart
@@ -1,40 +1,41 @@
 // Default entrance method executed by all .dart files
 void main() {
-  
- //Abstract classes doesn’t allow creating instances of that class. Neither new nor default constructor
+ //Abstract classes don’t allow creating instances of that class. Neither new nor default constructor
  //Animal animal = new Animal();
  //Animal animal = Animal();
 
-  //A non-abstract class allow creating instances of classes with new
+  //A non-abstract class allow creating instances of classes
+  // 1. With new
   final perro = new Perro();
   perro.emitirSonido();
+  // 2. Without new
+  final perroWithoutNew = Perro();
+  perroWithoutNew.emitirSonido();
   
   final gato = new Gato();
   gato.emitirSonido();
-  
 }
-
 
 abstract class Animal {
   
-  int patas;
+  late int patas;
   
   void emitirSonido();
   
 }
 
 class Perro implements Animal{
-  
-  int patas;
-  int colas;
+  // It's required to implement the parent's attributes and methods, such as 'int patas' in this case
+  late int patas;
+  late int colas;
   
   void emitirSonido() => print('GUAUUUUU!!');
   
 }
 
 class Gato implements Animal {
-  
-  int patas;
+  // It's required to implement the parent's attributes and methods, such as 'int patas' in this case
+  late int patas;
   
   void emitirSonido() => print('MIAUUUU!!');
   
