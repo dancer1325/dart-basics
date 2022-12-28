94dd702998699243831975821e8cf232d42789e0
dancer1325 <alfredotic0809@gmail.com>
2022-12-28 12:30:25 +0100

    refactor(03-bool-condicion.dart) : add commentaries (Section2)

--- 03-bool-condicion.dart
+++ 03-bool-condicion.dart
@@ -1,18 +1,19 @@
 // Default entrance method executed by all .dart files
 void main() {
 
-  bool activado = true;
-  print(activado);
+  bool active = true;
+  print(active);
   
-  if ( !activado ) {
-    print('El motor esta funcionando');
+  if ( !active ) {
+    print('The engine is working');
   } else {
-    print('Está apagado');
+    // Next line, to use ' in String --> Use "
+    print("It's turned off");
   }
   
   // Same part of the code, but without sense since the variable is a boolean
-  if ( activado == true ) {
-    print('El motor esta funcionando');
+  if ( active == true ) {
+    print('The engine is working');
   } 
   
 }
