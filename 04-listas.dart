d79732fbbde870aa63337dd003fbd45014d6080f
dancer1325 <alfredotic0809@gmail.com>
2022-12-28 12:20:10 +0100

    refactor(04-listas.dart) : add commentaries based on notes taken (Section2)

--- 04-listas.dart
+++ 04-listas.dart
@@ -1,24 +1,25 @@
 // Default entrance method executed by all .dart files
 void main() {
   
-  //To create dynamic list without type specified
+  //Create dynamic list without type specified
   List dynamicList = [1,2,3,4,5];
-  dynamicList.add("Hola Alfredo");    // Another type of data can be added
+  dynamicList.add("Hi Alfredo");    // Another type of data can be added
   print('dynamicList $dynamicList');
 
   //Once specified the type of list, it's not possible to add another type of data
-  List<int> numeros = [1,2,3,4,5];
-  print( "numeros $numeros" );
-  //numeros.add("Hola Alfredo");
-  
-  numeros.add(6);
-  print( "numeros $numeros" );
+  List<int> numbers = [1,2,3,4,5];
+  print( "numbers $numbers" );
+  //numbers.add("Hello Alfredo");  --> Error in code time
+
+  // If you don't fix the size --> You can add more elements
+  numbers.add(6);
+  print( "numbers $numbers" );
   
   // Fix size
   // 1) Problems with default_list_constructor when the null safety is enabled
   // https://dart.dev/tools/diagnostic-messages?utm_source=dartdev&utm_medium=redir&utm_id=diagcode&utm_content=default_list_constructor#default_list_constructor
   // 1.1] Without using new keyword
-  // List masNumeros = List(10);
+  // List moreNumbers = List(10);
   // 1.2] Using new keyword
   //  List anotherWay = new List(10);
 
@@ -29,6 +30,7 @@ void main() {
   // An error in run time is got if we try to add another element
   // list.add(2);
 
+  // Possible to modify the value of an element
   list[0] = 1;
   print( "list $list" );
   
