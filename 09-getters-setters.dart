f1d0f5d011a908f246b01669e01d4d2e19e427fd
dancer1325 <alfredotic0809@gmail.com>
2022-12-28 19:48:05 +0100

    refactor(09-getters-setters.dart) : add commentaries and fix typo (Section2)

--- 09-getters-setters.dart
+++ 09-getters-setters.dart
@@ -1,23 +1,24 @@
 // Default entrance method executed by all .dart files
 void main() {
   
-  final cuadrado = new Cuadrado();
+  final square = new Cuadrado();
 
-  // Invoke the setter method, just via '.NameOfTheSetterMethod'
-  cuadrado.lado = 10;
-  print( "cuadrado after cuadrado.lado $cuadrado" );
+  // Invoke the setter method -- 'InstanceOfTheClass.NameOfTheSetterMethod = ValueArgumentToPass'  --
+  square.side = 10;
+  print( "square after square.side $square" );
 
   // You can name the setterMethod as you wish
-  cuadrado.randomSetter = 90;
-  print( "cuadrado after cuadrado.randomSetter $cuadrado" );
+  square.randomSetter = 90;
+  print( "square after square.randomSetter $square" );
 
   // If a private attribute hasn't got setter method --> You can't pass the value
-  // cuadrado.attributeWithoutSetter
+  // square.attributeWithoutSetter
 
-  //Execution is stopped if we throw an exception
-  // cuadrado.lado = 0;
+  // Execution is stopped if something throws an exception
+  // square.lado = 0;
 
-  print( 'cuadrado.area: ${ cuadrado.area }' );
+  // Invoke the getter method -- 'InstanceOfTheClass.NameOfTheGetterMethod --
+  print( 'square.area: ${ square.area }' );
   
 }
 
@@ -25,16 +26,16 @@ class Cuadrado {
 
   // _    Indicates that it's a private attribute
   // https://dart.dev/tools/diagnostic-messages?utm_source=dartdev&utm_medium=redir&utm_id=diagcode&utm_content=not_initialized_non_nullable_instance_field#not_initialized_non_nullable_instance_field
-  late double _lado;
+  late double _side;
   late double _random = 90.6;
   late String _attributeWithoutSetter = 'Alfredo is handsome';
   
   // Expose the attribute of public way
-  set lado( double valor ) {
+  set side( double valor ) {
     if ( valor <= 0 ) {
-      throw('El lado no puede ser menor o igual a 0');
+      throw("Side of a square can't be lower or equal than 0");
     }
-    _lado = valor;
+    _side = valor;
   }
 
   // Any name can be given to setter and getter methods
@@ -42,21 +43,22 @@ class Cuadrado {
     _random = randomValue;
   }
 
-  // Getter method to get the are
+  // Getter method to get the area
   // 1. Classic
-  // double get area {return _lado * _lado;}
+  // double get area {return _side * _side;}
   // 2. Arrow function
-  double get area => _lado * _lado;
+  double get area => _side * _side;
   //Although _ is to indicate that it's private, without it, the variable isn't found
   //double get area2 => lado * lado;
 
   // If we don't initialize all the attributes --> making the interpolation will throw an error
+  // Method invoked by default when the interpolation for a class is made
   String toString() {
-    final lado = checkIfDoubleVariableExists(this._lado);
+    final side = checkIfDoubleVariableExists(this._side);
     final random = checkIfDoubleVariableExists(this._random);
     final attributeWithoutSetter = checkIfVariableExists(this._attributeWithoutSetter);
 
-    return 'Lado: $lado, Random $random and attributeWithoutSetter $attributeWithoutSetter ';
+    return 'Side: $side, Random $random and attributeWithoutSetter $attributeWithoutSetter ';
   }
 
   //TODO: Create a method checking by generics
